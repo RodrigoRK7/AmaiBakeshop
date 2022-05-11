bfc403ea44497206e618ff651ad04b5d1932e72a
JuanPabdl <a00825316@itesm.mx>
2022-05-11 14:55:32 -0500

    Pag login

---
+++ amai/src/components/Login/Login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+class Login extends React.Component{
+    render() {
+        return(
+            <>
+                <Container >
+                    <Row>
+                        <Col lg={4} md={6} sm={12}>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Correo Electronico </Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    <Form.Text className="text-muted">
+                                     <br></br>We'll never share your email with anyone else.
+                                    </Form.Text>
+                                </Form.Group>
+                                <br></br>
+                                <Form.Group className="mb-3" controlId="formBasicPassword">
+                                    <Form.Label>Contraseña 
+                                        
+                                    </Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    }
+}
+
+export default Login;
