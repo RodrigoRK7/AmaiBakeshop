972321b26fc6ce3b235f05150dd3d68650acc6b9
Sergio Guasso Idalias <sguasso@live.com>
2022-05-12 10:32:51 -0500

    Login Bootstrap Demo

--- front/src/App.js
+++ front/src/App.js
@@ -3,14 +3,16 @@ import React from 'react'
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
 
-import {Products, Navbar} from './components';
+// import {Products, Navbar, Login} from './components';
+import Login from './components/Login/Login';
 
 // Se instalo la extension: dsznajder.es7-react-js-snippets
 const app = () => {
   return (
     <div>
-        <Navbar />
-        <Products />
+        {/* <Navbar />
+        <Products /> */}
+        <Login/>
     </div>
   )
 }
